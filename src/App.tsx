93bb56d3507e841cc08b75f464f172788994c1ab
danielzm05 <danielzmfcbarcelona@gmail.com>
2025-03-17 17:36:11 -0300

    add Markdown preview feature and update layout in README editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
+import MarkdownPreview from "@uiw/react-markdown-preview";
+import { useState } from "react";
+
 function App() {
+  const [source, setSource] = useState("");
+
+  const changeText = () => {
+    const codeTextarea = document.getElementById("code-textarea") as HTMLTextAreaElement;
+    setSource(codeTextarea.value);
+    console.log(source);
+  };
+
   return (
-    <main className="min-h-screen bg-bg">
-      <h1 className="text-text1 font-semibold text-2xl">README EDITOR</h1>
+    <main className="flex min-h-screen bg-bg">
+      <aside>
+        <nav>
+          <ul>
+            <li>TEXT</li>
+          </ul>
+        </nav>
+      </aside>
+      <textarea className="m-4 flex-1 bg-bg2" name="code" id="code-textarea" onChange={() => changeText()}></textarea>
+      <MarkdownPreview source={source} style={{ flex: 1, padding: 16 }} />
     </main>
   );
 }
